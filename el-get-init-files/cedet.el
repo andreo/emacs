fabfea6c24b7693a8f9557cbda0b11f7b0d3e669
andreo <andrey.torba@gmail.com>
2014-10-19 23:22:57 -0700

    global-semantic-stickyfunc-mode

--- el-get-init-files/cedet.el
+++ el-get-init-files/cedet.el
@@ -4,6 +4,7 @@
 (global-semantic-highlight-func-mode t)
 (global-semantic-idle-local-symbol-highlight-mode t)
 (global-semantic-show-parser-state-mode t)
+(global-semantic-stickyfunc-mode t)
 (global-semantic-idle-breadcrumbs-mode t)
 
 (define-key semantic-mode-map (kbd "M-j") 'semantic-ia-fast-jump)
